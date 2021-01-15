0db7b27afa7bf2e7e4adefb57586a43b880a2204
Colin Harzing <colinharzing@hotmail.com>
2021-01-15 16:35:44 +0100

    added firebase utils, included store authenticated users into our databse

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,32 @@ const config = {
     appId: "1:381103468821:web:338caa3e404a9e8ff2f91c"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`user/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
